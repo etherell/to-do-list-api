0693e27a89aaf1c612b6c969c18736362a955a26
etherell <kostyaetherell@gmail.com>
2022-02-19 11:56:07 +0200

    Added aditional fields to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@
 
 class Task < ApplicationRecord
   belongs_to :project
+  acts_as_list scope: :project
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_063012) do
+ActiveRecord::Schema.define(version: 2022_02_19_091505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(version: 2022_02_19_063012) do
     t.string "description", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_done", default: false
+    t.datetime "deadline", null: false
+    t.integer "position"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
